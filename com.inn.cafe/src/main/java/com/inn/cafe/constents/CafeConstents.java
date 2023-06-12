18b228e0e0ad6770463d17f0fcc1c637fbe47be2
Suraj Pal <pals7313@gmail.com>
2023-06-12 14:30:25 +0530

    initial commit for all code files

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstents.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.constents;
+
+public class CafeConstents {
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
+
+    public static final String INVALID_DATA = "Invalid data.";
+
+    public static  final String UNAUTHORIZED_ACCESS =  "Unauthorized access";
+
+    public static final String STORE_LOCATION = "D:\\DOWNLOAD\\ProjectPdf";
+
+
+}
